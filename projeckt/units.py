ca13c11234acbbee97833186b966c4edaefd17e3
GStorm888 <denchikevstr@icloud.com>
2024-06-11 13:51:16 +0300

    сделал функцию 'process input'

--- projeckt/units.py
+++ projeckt/units.py
@@ -4,6 +4,15 @@ class Player(pygame.sprite.Sprite):
     def __init__(self, image_file, position):
         pygame.sprite.Sprite.__init__(self)
         self.image_file = pygame.image.load(image_file).convert_alpha()
+        self.rect = self.image.get_rect(topleft=position)
+        self.move_x = 0
+        self.move_y = 0
         
     def render(self, window):
         window.blit(self.image, self.rect)
+
+    def process_input(self):
+        pos = pygame.mouse.get_pos()
+        self.rect.midtop = pos
+        if self.punching:
+            self.rect.move_ip(10, 10)
