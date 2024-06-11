f755f29454d85e083510e62b45e2a734fcbfafd6
GStorm888 <denchikevstr@icloud.com>
2024-06-11 13:43:08 +0300

    начал написание основного класса игры 'Game'

--- projeckt/game.py
+++ projeckt/game.py
@@ -1,18 +1,42 @@
 import pygame
-
+from units import Player
 class Game():
 
     def __init__(self):
         pygame.init()
 
+        self.WINDOW_WIDTH = 800
+        self.WINDOW_YEIGHT = 800
+        self.FPS = 60
+
+        self.main_window = pygame.display.set_mode((self.WINDOW_WIDTH,
+                                                     self.WINDOW_YEIGHT))
+        self.clock = pygame.time.Clock()
+        self.running = True
+        self.plyer = Player("C:\Users\RobotComp.ru\OneDrive\Документы\projeckt\projeckt\images\green_body.png")
+
+
     def process_input(self):
-        pass
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                self.running = False
+                return
 
     def update_game_state(self):
         pass
 
     def render(self):
-        pass
+        self.main_window.fill(pygame.color.THECOLORS(["white"]))
+        self.plyer.render(self.main_window)
+        pygame.display.update()
 
     def main_loop(self):
-        pass
+        while self.running:
+            self.process_input()
+            self.update_game_state()
+            self.render()
+            self.clock.tick(self.FPS)
+        pygame.quit()
+
+game = Game()
+game.game_loop
