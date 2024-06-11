6943e41e9d99f33c7c15ab80200c691a296905ca
GStorm888 <denchikevstr@icloud.com>
2024-06-11 13:07:55 +0300

    начал разработку игры

---
+++ projeckt/game.py
@@ -0,0 +1,18 @@
+import pygame
+
+class Game():
+
+    def __init__(self):
+        pygame.init()
+
+    def process_input(self):
+        pass
+
+    def update_game_state(self):
+        pass
+
+    def render(self):
+        pass
+
+    def main_loop(self):
+        pass
