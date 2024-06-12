43e3bb58431f8dc9019e95843d0d405b48930104
GStorm888 <denchikevstr@icloud.com>
2024-06-12 15:43:12 +0300

    написал класс Ground для фона

---
+++ projeckt/ground.py
@@ -0,0 +1,15 @@
+import pygame
+
+class Grid:
+
+    def __init__(self, surface, camera):
+        self.surface = surface
+        self.camera = camera
+        self.color = (230,240,240)
+
+    def draw(self):
+        zoom = self.camera.zoom
+        x, y = self.camera.x, self.camera.y
+        for i in range(0,2001,25):
+            pygame.draw.line(self.surface,  self.color, (x, i * zoom + y), (2001 * zoom + x, i * zoom + y), 3)
+            pygame.draw.line(self.surface, self.color, (i * zoom + x, y), (i * zoom + x, 2001 * zoom + y), 3)
